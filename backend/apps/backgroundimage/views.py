3017031dee0f7b51334e418aeff75bff73dbac37
reante patterson <reantepatterson@gmail.com>
2021-10-14 16:30:46 -0700

    added views to userbackground app

--- backend/apps/backgroundimage/views.py
+++ backend/apps/backgroundimage/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+class BackgroundImgList(generics.ListAPIView):
+    queryset = BackgroundImg.objects.all()
+    serializer_class = BackgroundImgSerializer
