37c5c000807d5e4a462e5ce86aff8e0fa3631bce
reante patterson <reantepatterson@gmail.com>
2021-10-15 15:32:15 -0700

    set urls for userbackground and made small changes to settings.py

--- backend/apps/backgroundimage/urls.py
+++ backend/apps/backgroundimage/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BackgroundImgList.as_view(), name= 'BackgroundImgList'),
+    path('', views.BackgroundImgList.as_view(), name= 'Background-images'),
 ]
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,6 +16,7 @@ import django_heroku
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -170,5 +171,9 @@ cloudinary.config(
 )
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS':
+    ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEAFULT_PAGINATION_CLASS':
+    'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 21
 }
