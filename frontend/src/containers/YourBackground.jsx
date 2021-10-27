d8aa28544038598d9619039ae0737633d3e03389
superstevy <superstevy@yahoo.com>
2021-10-27 19:26:10 -0400

    corrected minor error in YourBackground.jsx

--- frontend/src/containers/YourBackground.jsx
+++ frontend/src/containers/YourBackground.jsx
@@ -4,43 +4,44 @@ import group44 from "../assets/img/Mask Group 34.png";
 import addbtn from "../assets/img/+.svg";
 import Header from "../components/Common/Header";
 
-function YourBackground () {
-    return (
+function YourBackground() {
+  return (
     <>
-        <Header />
-        <div id="Yourbackground" class="yb-bg">
-            <div class="yb-body">
-                <main class="yb-main">
-
-                    <div class="YB-header">
-                        <p>Your Backgrounds</p>
-                    </div>
-
-                    <div class="scrolling-wrapper">
-                        <div class="card">
-                            <img src={group33} alt=""></img>
-                            <div class="link-panel">
-                                <a id="preview-btn">Preview | </a><button class="DL-btn">Download</button>
-                            </div>
-                        </div>
-
-                        <div class="card">
-                            <img src={group44} alt=""></img>
-                            <div class="link-panel">
-                                <a href="#">Preview | </a><button class="DL-btn">Download</button>
-                            </div>
-                        </div>
-
-                        <div class="card">
-                            <div class="add-btn">
-                                <img src={addbtn}></img>
-                            </div> 
-                        </div>
-                    </div>
-                </main>
+      <Header />
+      <div id="Yourbackground" class="yb-bg">
+        <div class="yb-body">
+          <main class="yb-main">
+            <div class="YB-header">
+              <p>Your Backgrounds</p>
             </div>
-        </div>
-        </>
-    );
 
+            <div class="scrolling-wrapper">
+              <div class="card">
+                <img src={group33} alt=""></img>
+                <div class="link-panel">
+                  <a id="preview-btn">Preview | </a>
+                  <button class="DL-btn">Download</button>
+                </div>
+              </div>
+
+              <div class="card">
+                <img src={group44} alt=""></img>
+                <div class="link-panel">
+                  <a href="#">Preview | </a>
+                  <button class="DL-btn">Download</button>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="add-btn">
+                  <img src={addbtn}></img>
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
 export default YourBackground;
