091df1ef403b8e41e00971e07000e56038f30dca
superstevy <superstevy@yahoo.com>
2021-10-28 17:28:14 -0400

    modified generate.jsx

--- frontend/src/containers/Generate.jsx
+++ frontend/src/containers/Generate.jsx
@@ -1,65 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import Steps from "../components/Common/Steps";
+import Header from "../components/Common/Header";
+import SelectBackground from "../components/Common/SelectBackground";
+import Logo from "./Logo";
+import Info from "./Info";
 
-import Roberto from "../assets/img/roberto-nickson-tleCJiDOri0-unsplash.png";
-import MG26 from "../assets/img/Mask Group 26.png";
-import Workspace from "../assets/img/myhq-workspaces-OhNSJMm9yJI-unsplash.png";
-import MG25 from "../assets/img/Mask Group 25.png";
-import MG27 from "../assets/img/Mask Group 27.png";
-import MG28 from "../assets/img/Mask Group 28.png";
-import MG29 from "../assets/img/Mask Group 29.png";
-import MG30 from "../assets/img/Mask Group 30.png";
+import API from "../API";
+
+const api = new API();
 
 function Generate() {
+  const [step, setStep] = useState(1);
+
+  const [inputBackgroundId, setInputBackgroundId] = useState(null);
+  const [inputCompanyLogo, setInputCompanyLogo] = useState(null);
+  const [inputUsername, setInputUsername] = useState(null);
+  const [inputCompanyName, setInputCompanyName] = useState(null);
+  const [inputRole, setInputRole] = useState(null);
+
+  const [previewCompanyLogoUrl, setPreviewCompanyLogoUrl] = useState(null);
+
+  const submitToGenerate = (e) => {
+    let params = {
+      username: inputUsername,
+      company_name: inputCompanyName,
+      company_logo: inputCompanyLogo,
+      role: inputRole,
+      background_id: inputBackgroundId,
+    };
+    api.postUserBackground(params);
+    e.preventDefault();
+  };
+
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return (
+          <SelectBackground
+            setStep={setStep}
+            inputBackgroundId={inputBackgroundId}
+            setInputBackgroundId={setInputBackgroundId}
+          />
+        );
+      case 2:
+        return (
+          <Logo
+            setStep={setStep}
+            setInputCompanyLogo={setInputCompanyLogo}
+            previewCompanyLogoUrl={previewCompanyLogoUrl}
+            setPreviewCompanyLogoUrl={setPreviewCompanyLogoUrl}
+          />
+        );
+      case 3:
+        return (
+          <Info
+            setStep={setStep}
+            inputUsername={inputUsername}
+            setInputUsername={setInputUsername}
+            inputRole={inputRole}
+            setInputRole={setInputRole}
+            inputCompanyName={inputCompanyName}
+            setInputCompanyName={setInputCompanyName}
+            submitToGenerate={submitToGenerate}
+          />
+        );
+    }
+  };
+
+  useEffect(() => {
+    console.log(inputBackgroundId);
+  }, [inputBackgroundId]);
+
   return (
     <div id="generate">
-      <main>
-        <Steps />
+      <Header />
 
-        <div className="choose">
-          <p>Choose your background Image</p>
-        </div>
-        <div className="flex-2">
-          <div className="bgd">
-            <img src={Roberto} alt="" />
-          </div>
-          <div className="bgd">
-            <img src={MG26} alt="" />
-          </div>
-          <div className="bgd">
-            <img src={Workspace} alt="" />
-          </div>
-          <div className="bgd">
-            <img src={MG25} alt="" />
-          </div>
-          <div className="bgd">
-            <img src={MG27} alt="" />
-          </div>
-          <div className="bgd">
-            <img src={MG28} alt="" />
-          </div>
-          <div className="bgd">
-            <img src={MG29} alt="" />
-          </div>
-          <div className="bgd">
-            <img src={MG30} alt="" />
-          </div>
-        </div>
-      </main>
-
-      <footer>
-        <div className="btn-1">
-          <a href="/logo" className="next">
-            Next
-          </a>
-        </div>
-        <div className="btn-2">
-          <a href="/" className="back">
-            Back
-          </a>
-        </div>
-      </footer>
+      {renderStep(
+        step,
+        setStep,
+        inputBackgroundId,
+        setInputBackgroundId,
+        inputCompanyLogo,
+        setInputCompanyLogo,
+        previewCompanyLogoUrl,
+        setPreviewCompanyLogoUrl,
+        inputUsername,
+        setInputUsername,
+        inputRole,
+        setInputRole,
+        inputCompanyName,
+        setInputCompanyName,
+        submitToGenerate
+      )}
     </div>
   );
 }
