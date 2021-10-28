b68697723a3ddb30fb9db576e3f6bf1dd9dc4722
superstevy <superstevy@yahoo.com>
2021-10-28 18:32:08 -0400

    added SelectBackground.jsx

--- frontend/src/components/Common/SelectBackground.jsx
+++ frontend/src/components/Common/SelectBackground.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-function SelectBackground() {
-  return <div></div>;
+import Steps from "./Steps";
+
+import { fetchBimg } from "../../reducks/backgroundimages/operations";
+import { getBimg } from "../../reducks/backgroundimages/selectors";
+
+function SelectBackground({
+  setStep,
+  inputBackgroundId,
+  setInputBackgroundId,
+}) {
+  const selector = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const bImg = getBimg(selector);
+
+  useEffect(() => {
+    dispatch(fetchBimg());
+  }, []);
+
+  return (
+    <div>
+      <main>
+        <Steps />
+
+        <div class="choose">
+          <p>Choose your background Image</p>
+        </div>
+        <div class="flex-2">
+          {bImg["results"] &&
+            bImg["results"].map((image) => (
+              <div class="bgd" key={image.id}>
+                <img
+                  src={image.image}
+                  alt=""
+                  onClick={() => setInputBackgroundId(image.id)}
+                />
+              </div>
+            ))}
+        </div>
+      </main>
+
+      <footer>
+        <div class="btn-1">
+          <button onClick={() => setStep(2)} class="next">
+            Next
+          </button>
+        </div>
+        <div class="btn-2">
+          <a href="/" class="back">
+            Back
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 export default SelectBackground;
